bcd4dffe2d4aff3064192aae790292eda15460a0
jeffcail <cljcailianjie@163.com>
2023-06-24 15:22:25 +0800

    gateway9527搭建测试

---
+++ cloud-gateway-gateway9527/src/main/java/com/jeffcail/springcloud/GatewayMain9527.java
@@ -0,0 +1,22 @@
+package com.jeffcail.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName GatewayMain9527
+ * @Description TODO
+ * @Author cc
+ * @Date 2023/6/24 2:56 下午
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+
+}
